530a745a83c863b792144ebe88f19e3f2590236b
Marian Morales <marianmoralesreyes6@gmail.com>
2023-10-30 10:03:25 -0700

    Actualización del programa

---
+++ proyecto tienda/CLASES/clestanteria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_tienda.CLASES
+{
+    internal class clestanteria
+    {
+        public int  EST_ID { get; set; }
+        public string EST_DESC  { get; set; }
+    }
+}
---
+++ proyecto tienda/CLASES/clgrupo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_tienda.CLASES
+{
+    internal class clgrupo
+    {
+        public int GRU_ID { get; set; }
+        public string GRU_NOMBRE { get; set; }
+    }
+}
